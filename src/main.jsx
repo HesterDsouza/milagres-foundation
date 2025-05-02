0e1f106c20c0c559b1d6b85e54bb83f475055c2f
Hester <hester.dsouza.08@gmail.com>
2025-05-02 15:00:16 +0530

    csr css, our work page fixes, url fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { HelmetProvider } from 'react-helmet-async'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
+
+const isDev = import.meta.env.DEV;
 
 createRoot(document.getElementById('root')).render(
   <HelmetProvider>
-    <HashRouter basename='/'>
-      <App />
-    </HashRouter>
+    {isDev ? (
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    ) : (
+      <HashRouter basename='/'>
+        <App />
+      </HashRouter>
+    )}
   </HelmetProvider>
 )
